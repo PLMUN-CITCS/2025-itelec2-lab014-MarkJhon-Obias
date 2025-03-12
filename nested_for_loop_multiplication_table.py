d66e4ae1dea04add2bf609beca3860f94d6bc6ff
MarkJhon-Obias <obiasmarkjhon_bsit@plmun.edu.ph>
2025-03-12 11:47:51 +0800

    Delete nested_for_loop_multiplication_table.py

--- nested_for_loop_multiplication_table.py
+++
@@ -1,9 +0,0 @@
-# MARKJOHN OBIAS
-# Week 04 - looping Statements
-# Laboratory # 14 - Guided Coding Exercise: Nested for Loop to Print a Multiplication Table
-
-for i in range(1, 6):
-    for j in range(1, 6):
-        product = i * j
-        print(f"{product:4}", end="")  # Prints with spacing for alignment
-    print()  # Moves to the next line after inner loop
